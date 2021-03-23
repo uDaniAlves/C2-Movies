b0ccce8aef44f0b29f3cb8b8002255939136b341
uDaniAlves <daniel.augusto.balves@gmail.com>
2021-03-23 01:46:12 +0000

    Challenge 2 Completed - Still room to improve

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,1 +1,30 @@
 declare module '*.svg'
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId:number;
+  setSelectedGenreId:function;
+  handleClickButton:function;
+  genres:GenreResponseProps[];
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre?.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+    )
 }
